9c478f882ea75ee125f23814ac644071f2df4651
farhangit3001 <94040292+farhangit3001@users.noreply.github.com>
2023-07-18 17:25:10 +0700

    Add files via upload

---
+++ mputbday/main.py
@@ -0,0 +1,14 @@
+import http.server
+import socketserver
+
+
+def run():
+    PORT = 8001
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
+
